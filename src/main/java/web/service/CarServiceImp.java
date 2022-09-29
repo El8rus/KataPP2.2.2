0ac71735c5bd3725f54b054316e0bcd0c889bb54
Elly <3.bayramova@gmail.com>
2022-09-29 21:18:29 +0500

    v1

---
+++ src/main/java/web/service/CarServiceImp.java
@@ -0,0 +1,27 @@
+package web.service;
+
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CarServiceImp implements CarService {
+    @Override
+    public List<Car> addCarList() {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("Tesla", 2020, "red"));
+        cars.add(new Car("Cadillac", 2015, "yellow"));
+        cars.add(new Car("Mercedes-Benz", 2013, "blue"));
+        cars.add(new Car("Porsche", 2019, "white"));
+        cars.add(new Car("BMW", 2015, "black"));
+        return cars;
+    }
+
+    @Override
+    public List<Car> getCarList(Integer count) {
+        return addCarList().stream().limit(count).collect(Collectors.toList());
+    }
+
+}
+
